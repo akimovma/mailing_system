d7114466429e0efe6567a4960283b3ee6c39e7a4
Michael Akimov <akimov.light.it@gmail.com>
2018-10-18 16:43:11 +0300

    check precommit hook

--- mailing/filters.py
+++ mailing/filters.py
@@ -5,19 +5,16 @@ from mailing.models import EmailTask
 
 
 class EmailTaskFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='icontains')
+    name = django_filters.CharFilter(lookup_expr="icontains")
 
     class Meta:
         model = EmailTask
-        fields = ['start_date', 'sent']
+        fields = ["start_date", "sent"]
 
 
 class EmailTemplateFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='icontains')
+    name = django_filters.CharFilter(lookup_expr="icontains")
 
     class Meta:
         model = EmailTemplate
-        fields = {
-            'subject': ['icontains'],
-            'created': ['exact'],
-        }
+        fields = {"subject": ["icontains"], "created": ["exact"]}
